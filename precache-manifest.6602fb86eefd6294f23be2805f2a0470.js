c0a7250dbe3836ffadf45233b2c466ebe071a956
gadgetDEV <50057607+gadgetDEV@users.noreply.github.com>
2020-05-30 01:16:18 +0200

    Updates

---
+++ precache-manifest.6602fb86eefd6294f23be2805f2a0470.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9026428a5ebd26d084691d0464e90f1a",
+    "url": "/cartermoviesearch/index.html"
+  },
+  {
+    "revision": "441a3276a61bb7c221cf",
+    "url": "/cartermoviesearch/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "bef27cb0f963a3321165",
+    "url": "/cartermoviesearch/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "441a3276a61bb7c221cf",
+    "url": "/cartermoviesearch/static/js/2.3af8e936.chunk.js"
+  },
+  {
+    "revision": "99d31cac8f650aa1f1c9574559942819",
+    "url": "/cartermoviesearch/static/js/2.3af8e936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef27cb0f963a3321165",
+    "url": "/cartermoviesearch/static/js/main.895de7d7.chunk.js"
+  },
+  {
+    "revision": "05feda3c749d10a86718",
+    "url": "/cartermoviesearch/static/js/runtime-main.01280eb5.js"
+  }
+]);
